84f02b756511b7ee097fbf459d7336ab0e839eb1
Ryunki Shin <ryunkishin@gmail.com>
2022-12-04 18:12:50 +0900

    getLeaderboard backend

--- backend/controllers/pgatour.js
+++ backend/controllers/pgatour.js
@@ -1,10 +1,11 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
+const axios = require('axios');
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
-const {PLAYERS,LEADERBOARD} = require('../url/pgatour')
-const headers = { headers: { 'Accept-Encoding': 'application/json',}}
+const { PLAYERS, LEADERBOARD } = require('../url/pgatour');
+const headers = { headers: { 'Accept-Encoding': 'application/json' } };
 
-const getPlayers = async (req,res,next) => {
+const getPlayers = async (req, res, next) => {
   let players = [];
   try {
     let x = await axios.get(PLAYERS, headers);
@@ -12,54 +13,93 @@ const getPlayers = async (req,res,next) => {
     /// for active players this year
     const $activePlayersList = $('.player-card.active');
     $activePlayersList.each((idx, node) => {
-      const firstname = $(node).find('.hidden-small > .player-firstname').text();
+      const firstname = $(node)
+        .find('.hidden-small > .player-firstname')
+        .text();
       const surname = $(node).find('.hidden-small > .player-surname').text();
       const country = $(node).find('.player-country-title').text();
       const image_src = $(node).find('img').attr('src'); //default picture path
       const image_file = $(node).find('img').attr('data-src'); //real picture file
       if (
-        country.localeCompare('korea, republic of', 'en', {sensitivity: 'base',}) === 0
+        country.localeCompare('korea, republic of', 'en', {
+          sensitivity: 'base',
+        }) === 0
       ) {
-        const default_headshot = image_src.split('/').at(-1) //to replace the default path
-        const image = image_src.replace(default_headshot, image_file)
+        const default_headshot = image_src.split('/').at(-1); //to replace the default path
+        const image = image_src.replace(default_headshot, image_file);
         players.push({
           country,
           name: firstname + ' ' + surname,
-          image
+          image,
         });
       }
-    })
+    });
     res.send(players);
   } catch (err) {
-    next(err)
+    next(err);
   }
 };
 
-const getLeaderboard = async(req,res,next)=>{
-  try{
-    let x = await axios.get(LEADERBOARD, headers);
-    const $ = cheerio.load(x.data);
-    console.log($)
-    // const $tounamentInfo = $('div.tounament-banner')
-    // console.log($tounamentInfo)
-    const $players = $('tbody > tr.line-row');
-    // const $players = $('.player-name-col');
-    console.log($players)
-    
-    $players.each((idx, node)=>{
-      console.log(idx)
-      // const player = $(node).find('player-name-col').text()
-      // console.log(player)
-      // const x = $(node).find('.player-name .KOR')
-      // if()
-      // console.log(x)
-    })
-    res.send("hey")
-  }catch(err){
-    next(err)
+const getLeaderboard = async (req, res, next) => {
+  try {
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(LEADERBOARD);
+
+    const d = '#stroke-play-container';
+    // const d = '#leaderboard-container';
+    await page.waitForSelector(d);
+    // const html = await page.$$('tbody tr.line-row .player-name-col');
+    // console.log(html)
+
+    const html2 = await page.$$eval('tr.line-row', (tr) => {
+      let all_rounds
+      
+      const results = tr.map((td, idx) => {
+        let round = td.querySelectorAll('.round-x')
+        
+        all_rounds = [...round].map((r, idx)=>{
+          return r.innerText
+        })
+        const score_board = {
+          total: td.querySelector('.total').innerText,
+          // tee_time: info.querySelector('.tee-time').innerText,
+          rounds: all_rounds,
+          strokes: td.querySelector('.strokes').innerText,
+        };
+        const final_result = {
+          position: td.querySelector('.position').innerText,
+          country: td.querySelector('.flag').getAttribute('aria-label'),
+          player: td.querySelector('.player-name-col').innerText,
+          score_board,
+        };
+        return final_result;
+      });
+      return results;
+    });
+    console.log('$$eval: ', html2);
+    // console.log("array: ",html2[0].score_board.rounds[0])
+    // const html2 = await page.$eval(d, el => el.outerHTML);
+    // console.log("$eval: ",html2)
+    // await page.click(x);
+
+    // const links = await page.evaluate(resultsSelector => {
+    //   return [...document.querySelectorAll(resultsSelector)].map(anchor => {
+    //     const title = anchor.textContent.split('|')[0].trim();
+    //     return `${title} - ${anchor.href}`;
+    //   });
+    // }, resultsSelector);
+    // console.log(links.join('\n'));
+    await browser.close();
+
+    res.send(html2);
+  } catch (err) {
+    next(err);
   }
-  console.log("leaderboard")
-} 
+};
+
 module.exports = {
-  getPlayers, getLeaderboard
+  getPlayers,
+  getLeaderboard,
 };
