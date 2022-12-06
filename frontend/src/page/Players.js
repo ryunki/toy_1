eaa967503420a8515ca0ced90dec416f3ce5289b
Ryunki Shin <ryunkishin@gmail.com>
2022-12-07 06:04:25 +0900

    show players with dropdown menu

--- frontend/src/page/Players.js
+++ frontend/src/page/Players.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
-import {Card, Col, Row} from 'react-bootstrap';
+import { Card, Col, Row, DropdownButton, Dropdown } from 'react-bootstrap';
+
+const showPlayer = {};
 
 const getPlayers = async () => {
   const { data } = await axios.get('/api/pgatour/players');
@@ -11,31 +13,62 @@ const getPlayers = async () => {
 
 const Players = () => {
   const [players, setPlayers] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState('');
+  const [showPlayer, setShowPlayer] = useState();
 
-useEffect(()=>{
-  getPlayers()
+  useEffect(() => {
+    getPlayers()
       .then((res) => {
         setPlayers(res);
-        console.log(res);
       })
       .catch((err) => console.log(err));
-},[])
+  }, []);
 
+  const showPlayerHandler = (player) => {
+    setSelectedPlayer(player);
+    players.map((item, idx) => {
+      if (player === item.name) {
+        setShowPlayer(item);
+      }
+    });
+  };
+  
   return (
     <>
-      <h2>Players</h2>
+      <Card.Title style={{fontSize:30}}>Players</Card.Title>
+      <br/>
       <Row xs={1} md={5} className="g-4">
-        {players.map((item, idx) => (
-          <Col key={idx}>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={item.image} alt="player"></Card.Img>
-              <Card.Body>
-                <Card.Title>{item.name}</Card.Title>
-                <Card.Text>{item.country}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+        <Col>
+          <DropdownButton id="dropdown-basic-button" title={selectedPlayer||"Select"}>
+            {players.map((item, idx) => (
+              <Dropdown.Item key={idx} onClick={() => showPlayerHandler(item.name)}>
+                {item.name}
+              </Dropdown.Item>
+            ))}
+          </DropdownButton>
+        </Col>
+
+        <Col>
+        {showPlayer && 
+          <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={showPlayer.image} alt="player"></Card.Img>
+            <Card.Body>
+              <Card.Title>{showPlayer.name}</Card.Title>
+              <Card.Text>{showPlayer.country}</Card.Text>
+            </Card.Body>
+          </Card>
+        }
+
+          {/* {players.map((item, idx) => (
+              <Card key={idx} style={{ width: '20rem'}}>
+                <Card.Img variant="top" src={item.image} alt="player"></Card.Img>
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Text>{item.country}</Card.Text>
+                </Card.Body>
+              </Card>
+          ))} */}
+        </Col>
       </Row>
     </>
   );
